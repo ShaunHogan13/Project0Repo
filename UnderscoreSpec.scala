02962c20f410bab91f2610280233819fa178bd6f
ShaunHogan13 <92875998+ShaunHogan13@users.noreply.github.com>
2021-10-20 13:28:48 -0500

    Third commit

---
+++ UnderscoreSpec.scala
@@ -0,0 +1,18 @@
+
+
+/*
+package example
+
+
+import org.scalaTest.FunSuite
+
+class UnderscoreSpec extends FunSuite{  
+    
+    
+    test("UnderscoreTest #1"){
+
+        val ints = (1 to 4).map(_ => "Int")
+        assert(ints == Vector("Int", "Int", "Int", "Int"))
+    }
+}
+*/
